fe2567cf2ec3a0cf3a7f727c5bb087e0059fc9e9
thiagocodin <sunnyzito@gmail.com>
2025-12-10 12:43:24 -0300

    Update loans.ts

--- src/api/loans.ts
+++ src/api/loans.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://biblioteca-backend-8nmn.onrender.com',
+  baseURL: 'https://biblioteca-backend-8nmn.onrender.com/api',
   withCredentials: true,
 });
 
